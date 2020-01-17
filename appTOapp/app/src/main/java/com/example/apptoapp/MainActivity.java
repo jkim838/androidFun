7bb43cd7f7e9c6cb301282760c27206ede1e841d
jkim838 <wirsindallestot@hotmail.com>
2020-01-18 01:32:43 +1300

    * appTOapp -- practice launching app from another app. possibly with some camera features as well

---
+++ appTOapp/app/src/main/java/com/example/apptoapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.apptoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int cookies = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView cookieCount = findViewById(R.id.mainText);
+        Button cookieButton = findViewById(R.id.cookies);
+
+
+        cookieButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cookies++;
+                cookieCount.setText(cookies);
+            }
+        });
+
+    }
+
+    // https://inducesmile.com/android/android-camera2-api-example-tutorial/
+}
