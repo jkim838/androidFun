0da37995b5a430e45f8b4fb9261c9cdbcf22a455
jkim838 <OKIM@powerprodllc.com>
2020-01-23 10:28:24 +1300

    * NDKExample - updated comments

--- NDKExample/app/src/main/cpp/native-lib.cpp
+++ NDKExample/app/src/main/cpp/native-lib.cpp
@@ -13,9 +13,13 @@ int cookies = 0;
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_jkim838_ndkexample_MainActivity_showCookie(JNIEnv *env, jclass instance)
 {
+    // Equivalent to printf
     __android_log_print(ANDROID_LOG_DEBUG, "COOKIE", "C++ COOKIES INT %d", cookies);
+    // crate a buffer for characters
     char buffer[32];
+    // cast integer to string and assign them to the buffer
     sprintf(buffer, "%d", cookies);
+    // return as string
     return env->NewStringUTF(buffer);
 }
 
@@ -23,7 +27,6 @@ Java_com_jkim838_ndkexample_MainActivity_showCookie(JNIEnv *env, jclass instance
 extern "C"
 JNIEXPORT jint JNICALL
 Java_com_jkim838_ndkexample_MainActivity_bakeCookie(JNIEnv *env, jclass clazz) {
-    // TODO: implement bakeCookie()
     cookies++;
     __android_log_print(ANDROID_LOG_DEBUG, "COOKIE", "COOKIE BAKED: %d", cookies);
     return cookies;
